308a6c089946aa91dbf7d73605276c492339fa52
Nahun Silva <nahuns@gmail.com>
2023-07-15 02:08:42 +0100

    Updated request service

--- frontend/src/views/public/Login.vue
+++ frontend/src/views/public/Login.vue
@@ -12,7 +12,10 @@
                   class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0"
               >
                 <div class="flex-auto px-4 lg:px-10 py-10">
-                  <form>
+
+                  <p v-if="errorMessage">{{ errorMessage }}</p>
+
+                  <form @submit.prevent="login">
                     <div class="relative w-full mb-3">
                       <label
                           class="block uppercase text-gray-700 text-xs font-bold mb-2"
@@ -22,6 +25,7 @@
                         type="email"
                         class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
                         placeholder="Email"
+                        v-model="email" required
                         style="transition: all 0.15s ease 0s;"
                     />
                     </div>
@@ -35,6 +39,7 @@
                         class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
                         placeholder="Password"
                         style="transition: all 0.15s ease 0s;"
+                        v-model="password" required
                     />
                     </div>
                     <div>
@@ -52,7 +57,7 @@
                     <div class="text-center mt-6">
                       <button
                           class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                          type="button"
+                          type="submit"
                           style="transition: all 0.15s ease 0s;"
                       >
                         Sign In
@@ -80,3 +85,35 @@
     </main>
   </div>
 </template>
+
+<script>
+
+import api from '../../services/api.js';
+
+export default {
+  data() {
+    return {
+      email: 'nahuns@gmail.com',
+      password: '1234567',
+      errorMessage: '',
+    };
+  },
+  methods: {
+    login() {
+      const { email, password } = this;
+      api
+          .post('/api/login', { email, password })
+          .then((response) => {
+            // Assuming the API response contains an access token
+            const accessToken = response.data.access_token;
+            localStorage.setItem('accessToken', accessToken);
+            this.$router.push('/todos');
+          })
+          .catch((error) => {
+            console.log(error)
+            this.errorMessage = error.response.data.message;
+          });
+    },
+  },
+};
+</script>
