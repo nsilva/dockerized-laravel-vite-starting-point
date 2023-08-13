b81106d0c877587e019cf024d07aaab158a6fde3
Nahun Silva <nahuns@gmail.com>
2023-08-13 05:15:33 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -Eeuo pipefail
 
 composer create-project laravel/laravel backend \
-  && cp backend .env.example .env \
-  && cp docker-scripts/Dockerfile backend/Dockerfile \
-  && cp docker-scripts/command.sh backend/command.sh \
+  && cp backend/.env.example backend/.env \
+  && cp docker-files/Dockerfile backend/Dockerfile \
+  && cp docker-files/command.sh backend/command.sh \
+  && cp -r crontab backend/crontab \
   && npm create vite@latest frontend
